46b1d1f01e9fe9c3827524b9ab099054fd80743f
Rasmus Kurušev <rakuru@ttu.ee>
2024-04-06 18:32:49 +0300

    Added ProductService

---
+++ e-commerce-api/e-commerce-api/Services/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using e_commerce_api.Models;
+
+namespace e_commerce_api.Services
+{
+    public interface IProductService
+    {
+        List<Product> GetProducts();
+    }
+}
---
+++ e-commerce-api/e-commerce-api/Services/ProductService.cs
@@ -0,0 +1,20 @@
+﻿using e_commerce_api.Models;
+
+namespace e_commerce_api.Services
+{
+    public class ProductService : IProductService
+    {
+        public List<Product> GetProducts()
+        {
+            var products = new List<Product>
+            {
+                new Product { Id = 123, Name = "Ketchup", Price = 0.45m },
+                new Product { Id = 456, Name = "Beer", Price = 2.33m },
+                new Product { Id = 879, Name = "Õllesnäkk", Price = 0.42m },
+                new Product { Id = 999, Name = "75\" OLED TV", Price = 1333.37m }
+            };
+
+            return products;
+        }
+    }
+}
