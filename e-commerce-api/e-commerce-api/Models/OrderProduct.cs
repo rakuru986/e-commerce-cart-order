71d677ad5de52e8451e15c49e443a4ac09d07136
Rasmus Kurušev <rakuru@ttu.ee>
2024-04-07 15:32:11 +0300

    Modified models

--- e-commerce-api/e-commerce-api/Models/OrderProduct.cs
+++ e-commerce-api/e-commerce-api/Models/OrderProduct.cs
@@ -2,11 +2,11 @@
 {
     public class OrderProduct
     {
-        public string Id { get; set; }
+        public string Id { get; set; } = Guid.NewGuid().ToString();
         public string Name { get; set; }
-        public decimal Price { get; set; }
+        public string Price { get; set; }
         public int ProductId { get; set; }
-        public int Quantity { get; set; }
+        public int? Quantity { get; set; }
         public Product ReplacedWith { get; set; }
     }
 }
